8ad189bc5a88f43e688fc980db30169f0adac0fb
Kelly Rauchenberger <fefferburbia@gmail.com>
2017-08-18 13:58:20 -0400

    Added copyright info to the top of three new files

--- gba/source/serialize.h
+++ gba/source/serialize.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright (C) 2017 hatkirby
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
 #ifndef POKEMON_H_67C60AC1
 #define POKEMON_H_67C60AC1
 
--- include/pokemon.h
+++ include/pokemon.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright (C) 2017 hatkirby
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
 #ifndef POKEMON_H_AD844D6F
 #define POKEMON_H_AD844D6F
 
